9868df97ee7e18a1bfb86604eaa0654d9aaea3c3
SaiqG <akkorsakov@gmail.com>
2022-11-16 18:32:57 +0300

    Beta finish vanilla js

--- js/app.js
+++ js/app.js
@@ -8,82 +8,27 @@ $(function () {
         $this.addClass("active");
         $("#list div").removeClass("active");
         $($(this).attr("active-menu")).addClass("active");
-        // console.log($(this).attr("active-menu"));
-        // console.log($($(this).attr("active-menu")))
 
     })
 });
 
 
-//js
-// document.querySelector('.stats__pos').addEventListener('change', (stats) => {
+//vanilla js
 
-//     document.getElementById(stats.target.id).addEventListener('change', () => {
-
-//         let stat = Math.round((+document.getElementById(stats.target.id).value - 10) / 2 - 0.1);
-//         let stt = "#" + stats.target.id + "Ch";
-//         let sts = "#" + stats.target.id + "Sub > div ";
-//         if (Math.sign(stat) == 1) {
-//             document.querySelector(stt).innerHTML = '+' + stat;
-
-//         }
-//         else {
-//             document.querySelector(stt).innerHTML = stat;
-//         }
-
-//         document.querySelectorAll(sts).forEach(element => {
-//             if (element.lastChild.checked) {
-//                 stat += 1;
-//                 if (Math.sign(stat) == 1) {
-//                     element.firstChild.textContent = '+' + stat;
-//                     stat -= 1;
-//                 }
-//                 else {
-//                     element.firstChild.textContent = stat;
-//                     stat -= 1;
-//                 }
-//             }
-//             else if (element.lastChild.indeterminate) {
-//                 stat += 2;
-//                 if (Math.sign(stat) == 1) {
-//                     element.firstChild.textContent = '+' + stat;
-//                     stat -= 2;
-//                 }
-//                 else {
-//                     element.firstChild.textContent = stat;
-//                     stat -= 2;
-//                 }
-//             }
-
-//             else {
-//                 if (Math.sign(stat) == 1) {
-//                     element.firstChild.textContent = '+' + stat;
-//                 }
-//                 else {
-//                     element.firstChild.textContent = stat;
-//                 }
-//             }
-
-//         });
-
-//     });
-
-// });
-// console.log(document.querySelectorAll("#Sub > div"))
-
-
-document.querySelector("#char").addEventListener('change', () => 
-{ 
+document.querySelector("#char").addEventListener('change', () => {
     RefreshStats();
 });
 
-function RefreshStats() {
+RefreshStats = () => {
     document.querySelectorAll("#Sub > div").forEach(element => {
+
         let stat = "#" + element.id[0] + element.id[1] + element.id[2];
         let id = stat + "Sub > div";
         let stt = stat + "Ch";
-        let mastery = +document.querySelector("#mastery").value
+        let mastery = +document.querySelector("#mastery").value;
+
         stat = Math.round((document.querySelector(stat).value - 10) / 2 - 0.1);
+
         if (Math.sign(stat) == 1) {
             document.querySelector(stt).innerHTML = '+' + stat;
 
@@ -105,7 +50,7 @@ function RefreshStats() {
                 }
             }
             else if (element1.lastChild.indeterminate) {
-                stat += mastery +1;
+                stat += mastery + 1;
                 if (Math.sign(stat) >= 0) {
                     element1.firstChild.textContent = '+' + stat;
                     stat -= mastery + 1;
@@ -127,26 +72,100 @@ function RefreshStats() {
         });
 
     });
+    document.querySelector("#mastery").value = "+" + +document.querySelector("#mastery").value;
 }
 
-function DoubleCheckbox(cb) {
+DoubleCheckbox = (cb) => {
     if (cb.readOnly) cb.checked = cb.readOnly = false;
     else if (!cb.checked) cb.readOnly = cb.indeterminate = true;
 }
 
-function ScrollMonst(){
+ScrollMonst = () => {
     document.querySelector("#second__screen").scrollIntoView({
         block: "center",
         inline: "nearest",
         behavior: "smooth"
     });
-    
+
 }
 
-function ScrollMain(){
+ScrollMain = () => {
     window.scroll({
         top: 0,
-        left:0,
+        left: 0,
         behavior: "smooth"
     });
 }
+
+const apiUrl = "https://www.dnd5eapi.co";
+
+FindMob = async () => {
+    try {
+        const monstersUrl = `${apiUrl}/api/monsters`
+        const response = await fetch(monstersUrl);
+        const data = await response.json();
+
+        return data;
+
+    } catch (error) {
+        alert("Ты опять обосрался ахахах 1")
+    }
+
+}
+
+
+ShowMob = async (select) => {
+    try {
+        const mobsList = await FindMob();
+        const count = mobsList.count;
+        let mobIndex;
+        if (select === -1) { mobIndex = Math.floor(Math.random() * count) }
+        else mobIndex = select.id;
+
+        document.querySelector(".monster__name").innerText = mobsList.results[mobIndex].name;
+
+        let mob = await (await fetch(apiUrl + mobsList.results[mobIndex].url)).json();
+
+        document.querySelector(".monster__hp").innerText = `${mob.hit_points} HP`;
+        document.querySelector(".monster__dmg").innerText = `${mob.hit_dice} dmg`;
+
+        if (mob.hasOwnProperty("image")) {
+            document.querySelector("#mob__pic").src = apiUrl + mob.image;
+        }
+        else document.querySelector("#mob__pic").src = "assets/img/unnamed.png";
+
+        document.querySelector(".mob__found").innerHTML = "";
+        search.value = "";
+
+    } catch (error) {
+        alert("Ты опять обосрался ахахах 2")
+    }
+}
+
+const form = document.querySelector("form");
+const search = document.querySelector(".mob__search");
+
+form.addEventListener("input", async (mob) => {
+    mob.preventDefault();
+    let count = 0;
+    document.querySelector(".mob__found").innerHTML = "";
+    const mobsList = await FindMob();
+    let mobs = mobsList.results;
+    let mobN = search.value;
+
+    mobs.forEach(name => {
+        if (name.name.match(mobN) && count < 5) {
+            console.log(mobs.indexOf(name));
+            count++;
+            document.querySelector(".mob__found").innerHTML +=
+                `<dt class="search__res" id="${mobs.indexOf(name)}" onclick="ShowMob(this)">${name.name}</dt>`
+        }
+        if (mobN == "") document.querySelector(".mob__found").innerHTML = "";
+
+    })
+})
+
+
+
+
+
